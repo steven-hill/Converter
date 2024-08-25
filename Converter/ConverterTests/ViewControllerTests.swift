da19580116de42b57af36096e4dce2f411267879
steven-hill <stevenhill15@icloud.com>
2024-08-25 22:12:20 +0100

    VC test. viewDidLoad() sets up unitType segmented control. Removed unitType private access control to make it accessible to tests.

--- Converter/ConverterTests/ViewControllerTests.swift
+++ Converter/ConverterTests/ViewControllerTests.swift
@@ -25,6 +25,19 @@ final class ViewControllerTests: XCTestCase {
         XCTAssertNotNil(sut.toUnit.delegate, "VC should be toUnit tableView's delegate.")
     }
     
+    func test_viewDidLoad_setsUpUnitTypeSegmentedControl() throws {
+        let sut = try makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.unitType.numberOfSegments, 5, "There should be 5 segments.")
+        XCTAssertEqual(sut.unitType.titleForSegment(at: 0), "Distance", "Segment at 0 should be 'Distance'.")
+        XCTAssertEqual(sut.unitType.titleForSegment(at: 1), "Duration", "Segment at 1 should be 'Duration'.")
+        XCTAssertEqual(sut.unitType.titleForSegment(at: 2), "Mass", "Segment at 2 should be 'Mass'.")
+        XCTAssertEqual(sut.unitType.titleForSegment(at: 3), "Temperature", "Segment at 3 should be 'Temperature'.")
+        XCTAssertEqual(sut.unitType.titleForSegment(at: 4), "Volume", "Segment at 4 should be 'Volume'.")
+    }
+    
     private func makeSUT() throws -> ViewController {
         let bundle = Bundle(for: ViewController.self)
         let sb = UIStoryboard(name: "Main", bundle: bundle)
